a7a5de66c53087be03c9d2fd353c6d53f72fac8f
AndyzAndrea <abarradas.mca17@lania.edu.mx>
2019-02-14 16:56:30 -0600

    oi,yjmunhbgtrvfcedfvgrthyujki

--- app/src/main/java/mx/com/lania/oamtemplate/AppDatabase.java
+++ app/src/main/java/mx/com/lania/oamtemplate/AppDatabase.java
@@ -10,6 +10,18 @@ import mx.com.lania.oamtemplate.Database.Dao.LearningObjectDao;
 import mx.com.lania.oamtemplate.Database.Dao.LessonDao;
 import mx.com.lania.oamtemplate.Database.Dao.QuestionDao;
 import mx.com.lania.oamtemplate.Database.Dao.ResultDao;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Acreedores;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Almacen;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Bancos;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.CapitalSocial;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Clientes;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.CostoDeVentas;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Depreciacion;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Edificios;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Maquinaria;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Proveedores;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Resultado;
+import mx.com.lania.oamtemplate.Database.Entities.EjercicioPerpetuo.Ventas;
 import mx.com.lania.oamtemplate.Database.Entities.Evaluation;
 import mx.com.lania.oamtemplate.Database.Entities.Example;
 import mx.com.lania.oamtemplate.Database.Entities.Exercise;
@@ -23,7 +35,28 @@ import mx.com.lania.oamtemplate.Database.Entities.Result;
  * Created by macbook on 05/03/18.
  */
 
-@Database(entities = {LearningObject.class, Lesson.class, Example.class, Exercise.class, Result.class, Evaluation.class, Question.class}, version = 1)
+@Database(entities = {LearningObject.class,
+        Lesson.class,
+        Example.class,
+        Exercise.class,
+        Result.class,
+        Evaluation.class,
+        Question.class,
+        /* --- Ejercicio Perpetuo ----------*/
+        Acreedores.class,
+        Almacen.class,
+        Bancos.class,
+        CapitalSocial.class,
+        Clientes.class,
+        CostoDeVentas.class,
+        Depreciacion.class,
+        Edificios.class,
+        Maquinaria.class,
+        Proveedores.class,
+        Resultado.class,
+        Ventas.class
+        /*-----------------------*/
+}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
 
     public static final String DB_NAME = "app_db";
---
+++ app/src/main/java/mx/com/lania/oamtemplate/Database/Dao/ResultadoDao.java
@@ -0,0 +1,4 @@
+package mx.com.lania.oamtemplate.Database.Dao;
+
+public interface ResultadoDao {
+}
